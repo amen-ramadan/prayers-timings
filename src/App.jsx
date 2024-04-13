9a2b62d047cb863603353f7a3beaf64510ddd268
amen <ramadanamen407@gmail.com>
2024-04-13 14:11:28 +0300

    respons item with the small screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
-import Button from "@mui/material/Button";
 import MainContent from "./components/MainContent";
 import { Container } from "@mui/material";
 
 function App() {
-	const [count, setCount] = useState(0);
 
 	return (
 		<>
